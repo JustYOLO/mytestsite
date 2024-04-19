3b87d225584f760fddbea83d546256b45cc98ad3
Just_YOLO <nascarf16@gmail.com>
2024-04-19 18:32:03 +0900

    final

--- todos/models.py
+++ todos/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Todo(models.Model):
+    # todo에 대한 데이터
+    
+    # 제목
     title = models.CharField(max_length=100)
+    # 완료 되었는지 확인하는 bool
     completed = models.BooleanField(default=False)
+    # 생성 날짜를 저장
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- todos/views.py
+++ todos/views.py
@@ -8,18 +8,24 @@ def index(request):
 
 # 새로운 todo 생성
 def create_todo(request):
+    # Post request를 받음
     title = request.POST.get('title')
+    # 받은 이름으로 todo 객체 생성
     todo = Todo.objects.create(title=title)
     todo.save()
     todos = Todo.objects.all().order_by('-id')
+    # 저장 후 반환
     return render(request, 'todo-list.html', {'todos': todos})
 
 # 완료한 todo 표시
 def mark_todo(request, pk):
+    # 해당하는 todo 객체를 가져옴
     todo = Todo.objects.get(pk=pk)
+    # 완료로 변경 (추후 toggle로 변경 고려)
     todo.completed = True
     todo.save()
     todos = Todo.objects.all().order_by('-id')
+    # 저장 후 반환
     return render(request, 'todo-list.html', {'todos': todos})
 
 # todo를 삭제
